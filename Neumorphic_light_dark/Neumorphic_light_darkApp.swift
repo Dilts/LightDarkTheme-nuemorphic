c735094a742acd4f336e16ccb3e54c346e0bbc97
Brian Dilts <brianedilts@gmail.com>
2021-12-23 19:05:37 -0700

    Initial Commit

---
+++ Neumorphic_light_dark/Neumorphic_light_darkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neumorphic_light_darkApp.swift
+//  Neumorphic_light_dark
+//
+//  Created by Brian Dilts on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Neumorphic_light_darkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
